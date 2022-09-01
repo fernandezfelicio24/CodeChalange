ba10ab0eee1e96ac46f5913b88f0ac0e74224661
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-09-01 21:38:19 +0900

    Start Challange 52 until 59

---
+++ Challenges/Challenges52-59.py
@@ -0,0 +1,138 @@
+"""
+
+Challange 52 until 59 is about WHILE LOOP
+
+
+"""
+
+
+def challange052():
+
+    """
+            Display a random integer between 1 and 100 inclusive.
+
+    """
+
+
+
+
+def challange053():
+
+    """
+        Display a random fruit from a list of five fruits.
+
+    """
+
+
+
+
+def challange054():
+
+    """
+
+            Randomly choose either heads or tails (“h” or “t”). Ask
+    the user to make their choice. If their choice is the same
+    as the randomly selected value, display the message
+    “You win”, otherwise display “Bad luck”. At the end, tell
+    the user if the computer selected heads or tails.
+    """
+
+
+
+
+
+
+
+def challange055():
+
+
+
+    """
+            Randomly choose a number between 1 and 5. Ask the user to pick a
+    number. If they guess correctly, display the message “Well done”,
+    otherwise tell them if they are too high or too low and ask them to pick a
+    second number. If they guess correctly on their second guess, display
+    “Correct”, otherwise display “You lose”.
+
+    """
+
+
+
+
+
+
+def challange056():
+
+
+
+    """
+        Randomly pick a whole number between 1
+    and 10. Ask the user to enter a number and
+    keep entering numbers until they enter the
+    number that was randomly picked.
+
+    """
+
+
+
+def challange057():
+
+    """
+    Update program 056 so that it tells the user if they are too high
+    or too low before they pick again.
+    """
+
+
+
+
+
+
+def challange058():
+
+    """
+            Make a maths quiz that asks five questions by randomly
+    generating two whole numbers to make the question
+    (e.g. [num1] + [num2]). Ask the user to enter the
+    answer. If they get it right add a point to their score. At
+    the end of the quiz, tell them how many they got correct
+    out of five.
+
+    """
+
+
+def challange059():
+
+    """
+            Display five colours and ask the user to pick one. If they
+    pick the same as the program has chosen, say “Well
+    done”, otherwise display a witty answer which involves
+    the correct colour, e.g. “I bet you are GREEN with envy”
+    or “You are probably feeling BLUE right now”. Ask
+    them to guess again; if they have still not got it right,
+    keep giving them the same clue and ask the user to
+    enter a colour until they guess it correctly.
+
+    """
+
+
+def main():
+
+
+    #challange052()
+    #challange053()
+    #challange054()
+    #challange055()
+    #challange056()
+    #challange057()
+    #challange058()
+    #challange058()
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
