de77201117d6812d8f8184aa58e14b9e54124967
fernandezfelicio24 <fernandesfelicio99@gmail.com>
2022-10-13 21:51:34 +0900

    Start Challenges80-87:String Manipulation

---
+++ Challenges/Challenges80-87.py
@@ -0,0 +1,152 @@
+"""
+
+Challange 80 until 87 is about STRING MANIPULATION
+
+
+"""
+import turtle
+
+def challange80():
+
+    """
+        Ask the user to enter their
+    first name and then display
+    the length of their first name.
+    Then ask for their surname
+    and display the length of
+    their surname. Join their first
+    name and surname together
+    with a space between and
+    display the result. Finally,
+    display the length of their full
+    name (including the space).
+
+    """
+
+
+
+def challange081():
+
+    """
+        Ask the user to type in their favourite school subject.
+    Display it with “-” after each letter, e.g. S-p-a-n-i-s-h-
+
+    """
+
+
+
+
+def challange082():
+
+    """
+        Show the user a line of text from your favourite poem
+    and ask for a starting and ending point. Display the
+    characters between those two points.
+
+    """
+
+
+
+
+
+
+def challange083():
+
+
+
+    """
+        Show the user a line of text from your favourite poem
+    and ask for a starting and ending point. Display the
+    characters between those two points.
+    """
+
+
+
+def challange084():
+
+
+
+    """
+
+        Ask the user to type in their
+    postcode. Display the first
+    two letters in uppercase.
+    """
+
+
+
+
+
+def challange085():
+
+    """
+        Ask the user to type in their name
+    and then tell them how many vowels
+    are in their name.
+    """
+
+
+
+
+
+
+
+
+def challange086():
+
+    """
+        Ask the user to enter a new password. Ask
+    them to enter it again. If the two passwords
+    match, display “Thank you”. If the letters are
+    correct but in the wrong case, display the
+    message “They must be in the same case”,
+    otherwise display the message “Incorrect”.
+
+    """
+
+
+def challange087():
+
+    """
+        Ask the user to enter a new password. Ask
+    them to enter it again. If the two passwords
+    match, display “Thank you”. If the letters are
+    correct but in the wrong case, display the
+    message “They must be in the same case”,
+    otherwise display the message “Incorrect”.
+
+    """
+
+
+
+
+
+
+def challange088():
+
+    """
+
+
+    """
+
+
+
+
+def main():
+
+    challange080()
+    #challange081()
+    #challange082()
+    #challange083()
+    #challange084()
+    #challange085()
+    #challange086()
+    #challange087()
+    #challange088()
+
+
+
+
+
+if __name__ == "__main__":
+    main()
